d61931db4f5da0b0d7d06b59bed739d259037b05
M.X <hkdjcmc@msn.cn>
2019-04-24 23:22:04 +0800

    Add DouBan_Apps_Remove.js

---
+++ Tampermonkey/DouBan_Apps_Remove.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         DouBan_Apps_Remove
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  删除豆瓣我的收藏的应用
+// @author       You
+// @match        https://www.douban.com/people/VVIVXIV/apps?action=collect
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    let deleteEle = document.querySelector('#content > div.grid-16-8.clearfix > div.article > div.app-list > div:nth-child(1) > div.content > div.user-operation > a.j.a_confirm_link');
+    let itemUrl = deleteEle.href;
+
+    let http = new XMLHttpRequest();
+    http.open("GET", itemUrl);
+    http.send();
+
+    location.reload();
+
+})();
